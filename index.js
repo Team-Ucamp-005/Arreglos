a14a7b2525cc062d936bbf3799f910a1076c2a3e
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-03-19 11:54:53 -0700

    ejercicios

--- index.js
+++ index.js
@@ -95,9 +95,9 @@ console.log('Funciona');
 
 // Reduce
 
-const lista = [10, 20, 30, 40].reduce(function (a, b) {
-  return a + b
-})
+// const lista = [10, 20, 30, 40].reduce(function (a, b) {
+//   return a + b
+// })
 /*
 vuelta 1
 a = 10
@@ -118,8 +118,74 @@ a + b
 */
 
 
-console.log(lista)
+// console.log(lista)
 
+
+
+// ejercicios 
+
+// Usando el operador for realiza la tabla de multiplicar de 2 Ejemplo 2 x 1 = 2 2 x 2 = 4 2 x 3 = 6 .... 2 x 10 = 20
+
+
+// for(let i = 1; i <=10; i++) {
+//   console.log(`2 x ${i} = ${i * 2}`);
+// }
+
+
+// Crear 2 arreglos como los siguientes const nombres = ['alberto', 'paty', 'Jose', 'daniel', 'luis', 'antonio', 'Luis', 'paty', 'luis'];
+
+// const nombreMayusculas = [];
+
+// Utiliza la función forEach para recorrer cada elemento del arreglo nombres. En cada iteración debes agregar el nombre en mayusculas en el arreglo nombreMayusculas
+
+// Usando un console.log, imprime el resultado. Debes tener el siguiente resultado ALBERTO, PATY, JOSE, DANIEL, LUIS, ANTONIO, LUIS, PATY, LUIS
+// const nombres = ['alberto', 'paty', 'Jose', 'daniel', 'luis', 'antonio', 'Luis', 'paty', 'luis', 'Antonio'];
+
+// const nombreMayusculas = [];
+
+// nombres.forEach(function (nombre){
+//   nombreMayusculas.push(nombre.toUpperCase())
+// })
+
+// console.log('mayuscula->', nombreMayusculas)
+
+
+// // Crea una nueva variable con el nombre nombreMinusculas. Para esta parte vas a utilizar la función map. El objetivo es poder crear un nuevo arreglo a partir del arreglo nombreMayusculas. Este nuevo arreglo debe contener ahora los nombre en minusculas.
+// let nombreMinusculas = nombreMayusculas.map(function (minuscula){
+//   return minuscula.toLowerCase()
+// })
+
+// console.log('minuscula->', nombreMinusculas)
+
+
+// // Crea una variable con el nombre filtroNombres. Vas a utilizar el arreglo nombres para esta tarea. Debes utilizar la función filter para que solo obtengas los nombres que sean luis Usa un console.log para ver el resultado. Debes tener como resultado lo siguiente luis, Luis, luis
+
+
+// let filtroNombres = nombres.filter(function (nombre) {
+//   return nombre.toLowerCase() === 'luis'
+// })
+
+// console.log('filtro', filtroNombres)
+
+
+// let findNombre = nombres.find(function(nombre) {
+//   console.log(nombre)
+//   return nombre.toLowerCase() === 'antonio'
+// })
+
+// console.log(findNombre)
+
+
+// Crea un nuevo arreglo con el nombre numeros y agrega los sigiguiente valores: const numeros = [2,4,6,8,10]
+
+// Crea una nueva variable con el nombre total. El objetivo es que uses la función reduce la cual debe sumar todos los valores del arreglo numeros. Imprime el resultado de la variable total. El resultado debe ser 30
+
+// const numeros = [2,4,6,8,10] 
+// let total = numeros.reduce(function (a, b) {
+//   return a + b
+// })
+    
+// console.log(total)
 
 
 
